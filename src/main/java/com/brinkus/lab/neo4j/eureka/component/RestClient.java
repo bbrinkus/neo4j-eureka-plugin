21adc0d3cb8fc6bfa24abd086ba3cf936bc82512
Balazs Brinkus <balazs.brinkus@nordnet.se>
2016-08-24 00:09:54 +0200

    Use the implementation of the rest client.

--- src/main/java/com/brinkus/lab/neo4j/eureka/component/RestClient.java
+++ src/main/java/com/brinkus/lab/neo4j/eureka/component/RestClient.java
@@ -1,30 +1,175 @@
 package com.brinkus.lab.neo4j.eureka.component;
 
+import com.brinkus.lab.neo4j.eureka.exception.RequestFailedException;
+import com.brinkus.lab.neo4j.eureka.exception.ResponseCodeNotMatchingException;
+import com.brinkus.lab.neo4j.eureka.exception.ResponseProcessFailedException;
 import com.brinkus.lab.neo4j.eureka.exception.RestClientException;
+import org.apache.http.Consts;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.routing.HttpRoutePlanner;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.DefaultRoutePlanner;
+import org.apache.http.impl.conn.DefaultSchemePortResolver;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public interface RestClient {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
-    int STATUS_OK = 200;
+public class RestClient {
 
-    int STATUS_NO_CONTENT = 204;
+    public static final class Builder {
 
-    String getHost();
+        private String host;
 
-    String get(final String uri) throws RestClientException;
+        private int port;
 
-    String get(final String uri, final int status) throws RestClientException;
+        public Builder withHost(final String host) {
+            this.host = host;
+            return this;
+        }
 
-    String post(final String uri, final String content) throws RestClientException;
+        public Builder withPort(final int port) {
+            this.port = port;
+            return this;
+        }
 
-    String post(final String uri, final String content, final int status) throws RestClientException;
+        public RestClient build() {
+            List<BasicHeader> defaultHeaders = Arrays.asList(new BasicHeader(HttpHeaders.ACCEPT, "application/json"));
 
-    String put(final String uri) throws RestClientException;
+            PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+            connectionManager.setDefaultMaxPerRoute(10);
+            connectionManager.setMaxTotal(20);
 
-    String put(final String uri, final int status) throws RestClientException;
+            HttpRoutePlanner routePlanner = new DefaultRoutePlanner(DefaultSchemePortResolver.INSTANCE) {
+                @Override
+                public HttpRoute determineRoute(final HttpHost httpHost, final HttpRequest request, final HttpContext context) throws HttpException {
+                    HttpHost target = httpHost != null ? httpHost : new HttpHost(host, port, "http");
+                    return super.determineRoute(target, request, context);
+                }
+            };
 
-    String delete(final String uri) throws RestClientException;
+            HttpClientBuilder builder = HttpClientBuilder
+                    .create()
+                    .setRoutePlanner(routePlanner)
+                    .setDefaultHeaders(defaultHeaders)
+                    .setConnectionManager(connectionManager);
 
-    String delete(final String uri, final int status) throws RestClientException;
+            return new RestClient(host, port, builder.build());
+        }
+
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestClient.class);
+
+    public static final int STATUS_OK = 200;
+
+    public static final int STATUS_NO_CONTENT = 204;
+
+    public static final String NO_CONTENT = "";
+
+    private final String host;
+
+    private final int port;
+
+    private final HttpClient httpClient;
+
+    RestClient(final String host, final int port, final HttpClient httpClient) {
+        this.host = host;
+        this.port = port;
+        this.httpClient = httpClient;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String get(final String uri) throws RestClientException {
+        return get(uri, STATUS_OK);
+    }
+
+    public String get(final String uri, final int status) throws RestClientException {
+        HttpGet request = new HttpGet(uri);
+        return sendRequests(request, status);
+    }
+
+    public String post(final String uri, final String content) throws RestClientException {
+        return post(uri, content, STATUS_OK);
+    }
+
+    public String post(final String uri, final String content, final int status) throws RestClientException {
+        HttpPost request = new HttpPost(uri);
+        request.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        request.setEntity(new StringEntity(content, Consts.UTF_8));
+
+        return sendRequests(request, status);
+    }
+
+    public String put(final String uri) throws RestClientException {
+        return put(uri, STATUS_OK);
+    }
+
+    public String put(final String uri, final int status) throws RestClientException {
+        HttpPut request = new HttpPut(uri);
+        return sendRequests(request, status);
+    }
+
+    public String delete(final String uri) throws RestClientException {
+        return delete(uri, STATUS_OK);
+    }
+
+    public String delete(final String uri, final int status) throws RestClientException {
+        HttpDelete request = new HttpDelete(uri);
+        return sendRequests(request, status);
+    }
+
+    private String sendRequests(HttpUriRequest request, int httpStatus) throws RestClientException {
+        HttpResponse response;
+        try {
+            response = httpClient.execute(request);
+        } catch (IOException e) {
+            String message = "An error occurred during the registration HTTP communication process!";
+            LOGGER.error(message, e);
+            throw new RequestFailedException(message, e);
+        }
+        if (response.getStatusLine().getStatusCode() != httpStatus) {
+            String message = String.format("The response status code %s is not matching with the expected %s!", response.getStatusLine().getStatusCode(), host);
+            LOGGER.warn(message);
+            throw new ResponseCodeNotMatchingException(message);
+        }
+
+        try {
+            if (response.getStatusLine().getStatusCode() == STATUS_OK) {
+                return EntityUtils.toString(response.getEntity());
+            } else {
+                return NO_CONTENT;
+            }
+        } catch (IOException e) {
+            String message = "An error occurred during the response process!";
+            LOGGER.error(message, e);
+            throw new ResponseProcessFailedException(message, e);
+        }
+    }
 
 }
-
